b5176a92521f64bf7f3343520974dd17cf6b13db
Konstantin Kobliuck <kkobliuk@gmail.com>
2023-02-03 17:00:40 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let inputArr = str.split('');
+
+  for (let i = 0; i < inputArr.length; i++) {
+    let currentConfig = bracketsConfig.find(arr => arr.includes(inputArr[i])); 
+    if(!currentConfig) return false; 
+
+    let nextBracket = currentConfig[currentConfig.length-1];
+    if (inputArr[i + 1] === nextBracket) {
+      inputArr.splice(i, 2);
+      i = -1;
+    }
+  }
+  return !inputArr.length;
 }
